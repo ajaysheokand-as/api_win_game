3d718440dfa6c24b96da5c7dca46aea1c9e652d4
Abhishek Dhull <abhishek.dhull@affluencesolutions.com>
2022-12-05 18:34:24 +0530

    config

---
+++ payment/config copy.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_live_NBCjFGtgL5ANF9';
+$keySecret = 'KfiLy99cNiQp2V494fphgsVf';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
